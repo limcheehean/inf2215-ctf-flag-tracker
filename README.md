78c00625ef3c4b88b2b2ca6fc5be26501467e58d
Lim Chee Hean <lychee0504@gmail.com>
2024-04-09 04:22:54 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,22 @@
-# Getting Started with Create React App
+# ICT2215 CTF Flag Tracker
+Never lose track of your flags again!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+---
+## How to get?
 
-## Available Scripts
+Download the zip file from [here](release/CtfFlagTracker.zip), unzip, click on index.html and you are ready to go!
 
-In the project directory, you can run:
+If you don't trust me, clone the repository and run build it yourself 😒.
 
-### `npm start`
+---
+## How to use?
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. Enter all the flag codes (the 3 character code in the question) in the **Add Flag** section.
+2. Add you solve the CTF, enter your solution in the **Solve Flag** section.
+3. If a valid solution is entered, it will automatically be displayed in the table under the **My Flags** section.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+![Image](flag-tracker.png)
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+---
+## Disclaimer
+The author is not liable for any loss of flags or marks from the use of this tracker.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Divider, Input, Space, Table} from "antd";
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [flagCodeInput, setFlagCodeInput] = useState("")
+  const [solutionInput, setSolutionInput] = useState("")
+  const [solutionResult, setSolutionResult] = useState("")
+  const [solutionHash, setSolutionHash] = useState("")
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem("data") || "[]"))
+    console.log(data)
+  }, [])
+
+  useEffect(() => {
+    if (data !== null)
+      saveData()
+  }, [data])
+
+  const saveData = () => {
+    localStorage.setItem("data", JSON.stringify(data))
+  }
+
+  const sha256Hash = async (text) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(text)
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("")
+  }
+
+  const addFlagCode = () => {
+    if (flagCodeInput.length !== 3)
+      return
+    if (data.find(d => d.flagCode === flagCodeInput)) {
+      setFlagCodeInput("")
+      return
+    }
+    setData([...data, {"flagCode": flagCodeInput, "key": flagCodeInput}])
+    setFlagCodeInput("")
+  }
+
+  const removeFlag = (flagCode) => {
+    setData(data.filter(d => d.flagCode !== flagCode))
+  }
+
+  const testSolution = () => {
+    if (solutionInput.trim() === "") {
+      setSolutionInput("")
+      setSolutionHash("")
+      setSolutionResult("")
+      return
+    }
+    sha256Hash(solutionInput).then(hash => {
+      const flagCode = hash.substring(0, 3)
+      const flag = hash.substring(3, 16)
+      setSolutionHash("Flag " + flagCode + ": " + flag)
+      const matchedFlag = data.find(d => d.flagCode === flagCode)
+      if (matchedFlag === undefined) {
+        setSolutionResult("Solution does not produce matching flag codes")
+        return
+      }
+      setSolutionResult("Congratulations, you have found a flag!")
+      setData(data.map(item => {
+        if (item.flagCode === flagCode) {
+          item.flag = flag
+          item.solution = solutionInput
+        }
+        return item
+      }))
+      setSolutionInput("")
+    })
+  }
+
+  const columns = [
+    {
+      title: "Flag Code",
+      dataIndex: "flagCode",
+      key: "flag_code"
+    },
+    {
+      title: "Flag",
+      dataIndex: "flag",
+      key: "flag"
+    },
+    {
+      title: "Solution",
+      dataIndex: "solution",
+      key: "solution"
+    },
+    {
+      title: "Action",
+      dataIndex: "flagCode",
+      key: "action",
+      render: (text) => (
+          <Button onClick={() => removeFlag(text)}>Remove</Button>
+      )
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>INF2215 CTF Flag Tracker</h1>
+      <p>Never lose track of your flags again!</p>
+      <Divider/>
+      <div>
+        <h3>Add Flag</h3>
+        <p>Add the 3 letter flag code found on the questions here.</p>
+        <Space.Compact>
+          <Input placeholder='Flag Code "4c6"' value={flagCodeInput} onChange={e => setFlagCodeInput(e.target.value)} onKeyDown={e => {if (e.key === "Enter") addFlagCode()}}/>
+          <Button type="primary" onClick={() => addFlagCode()}>Add</Button>
+        </Space.Compact>
+      </div>
+      <div>
+        <h3>Solve Flag</h3>
+        <p>Enter the solution to any flag here.</p>
+        <Space.Compact>
+          <Input placeholder='Flag Solution "denver"' value={solutionInput} onChange={e => setSolutionInput(e.target.value)} onKeyDown={e => {if (e.key === "Enter") testSolution()}}/>
+          <Button type="primary" onClick={() => testSolution()}>Submit</Button>
+        </Space.Compact>
+        <p style={{fontWeight: "bold"}}>{solutionHash}</p>
+        <p style={{marginBottom: "24px"}}>{solutionResult}</p>
+      </div>
+      <div>
+        <h3>My Flags</h3>
+        <Table dataSource={data} columns={columns} />
+      </div>
     </div>
   );
 }
